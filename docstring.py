c6f4f943200a259636b7d2c13192d9e5deec0b49
AnkushDhekane <117856090+AnkushDhekane@users.noreply.github.com>
2023-02-09 10:37:01 +0530

    Add files via upload

---
+++ docstring.py
@@ -0,0 +1,13 @@
+def docstring():
+	"""These function gives you information about python documentation.
+	i.e. how to access function information in easy way
+	 -By using function.__doc__ method
+	 -By using help(function)"""
+
+	return None
+
+print("Using __doc__:")
+print(docstring.__doc__)
+
+print("Using help:")
+help(docstring)
